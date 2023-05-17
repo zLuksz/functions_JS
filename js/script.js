c14f8f8b8323f91b99224dd8e52b9293adb42fca
Lucas Santos <lucas.souza_santos@live.com>
2023-05-17 13:42:35 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function soma(n1, n2) {
+    //return n1 + n2;
+    alert(n1 + n2)
+}
+function subtracao(n1, n2) {
+    //return n1 - n2;
+    alert(n1 - n2)
+}
+function multiplicacao(n1, n2) {
+    //return n1 * n2;
+    alert(n1 * n2)
+}
+function divisao(n1 ,n2) {
+    //return n1 / n2;
+    alert(n1 / n2)
+}
+
+function mostrarnome() {
+    let nome = prompt('Digite Seu Nome:')
+    alert('Hello, ' + nome)
+}
+
+function retangulo(base, altura) {
+    alert(base * altura);
+}
+function areacirculo(pi, raio) {
+    alert(pi*(raio*raio))
+}
