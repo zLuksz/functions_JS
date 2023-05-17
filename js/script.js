ed4eb9c7bf6cae070118ba765060cd1bad948d6c
Lucas Santos <lucas.souza_santos@live.com>
2023-05-17 17:26:36 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,38 +1,9 @@
-function soma(n1, n2) {
-    //return n1 + n2;
-    alert(n1 + n2);
-}
-function subtracao(n1, n2) {
-    //return n1 - n2;
-    alert(n1 - n2);
-}
-function multiplicacao(n1, n2) {
-    //return n1 * n2;
-    alert(n1 * n2);
-}
-function divisao(n1 ,n2) {
-    //return n1 / n2;
-    alert(n1 / n2);
-}
-
-function mostrarnome() {
-    let nome = prompt('Digite Seu Nome:');
-    alert('Hello, ' + nome);
-}
-
-function retangulo(base, altura) {
-    alert(base * altura);
-}
-function areacirculo(pi, raio) {
-    alert(pi*(raio*raio));
-}
-
 // CALCULAR CIRCULO
 
 const pi = 3.14
 
 function calculararea_circulo(){
-    let raio = prompt('Informe o raio: ');
+    var raio = parseInt(document.getElementById('raio').value);
     let area = (pi * raio* raio);
     alert('A area do circulo e: ' + area);
 }
@@ -40,8 +11,8 @@ function calculararea_circulo(){
 // CALCULAR AREA TRIANGULO
 
 function area_triangulo(){
-    let cateto1 = prompt('Digite o valor 1: ');
-    let cateto2 = prompt('Digite o valor 2: ');
+    var cateto1 = parseInt(document.getElementById('cateto1').value);
+    var cateto2 = parseInt(document.getElementById('cateto2').value);
     let hipotenusa = Math.sqrt((cateto1 * cateto2) + (cateto1 * cateto2));
     alert('Sua Hipotenusa é: ' + hipotenusa);
 }
@@ -49,8 +20,8 @@ function area_triangulo(){
 // CONVERTER REAL EM DOLAR
 
 function real_dolar() {
-    let valor_cotacao = 4.94
-    let valor_reais = prompt('Informe o valor a ser convertido: ');
+    var valor_cotacao = parseInt(document.getElementById('valor_cotacao').value);
+    var valor_reais = parseInt(document.getElementById('valor_reais').value);
     let conversao = valor_cotacao * valor_reais;
     alert('O valor em dolares é: ' + conversao);
 }
@@ -58,7 +29,7 @@ function real_dolar() {
 // CONVERTER GRAUS CELCIUS EM FAHRENHEIT
 
 function graus_celcius() {
-    let graus_celcius = prompt('Insira A Temperatura De Hoje: ');
+    var graus_celcius = parseInt(document.getElementById('graus_celcius').value);
     fahrenheit = (graus_celcius * 1.8 + 32);
     alert('Temperatura Em Fahrenheit: ' + fahrenheit);
 }
@@ -66,9 +37,9 @@ function graus_celcius() {
 // CALCULAR MEDIA ALUNOS
 
 function media_alunos() {
-    let nota1 = parseFloat(prompt('1° Nota: '));
-    let nota2 = parseFloat(prompt('2° Nota: '));
-    let nota3 = parseFloat(prompt('3° Nota: '));
+    var nota1 = parseInt(document.getElementById('nota1').value);
+    var nota2 = parseInt(document.getElementById('nota2').value);
+    var nota3 = parseInt(document.getElementById('nota3').value);
     media_ponderada = (nota1 + nota2 + nota3) / 3
     alert('A Média Aritimetica é: ' + media_ponderada);
 }
@@ -76,9 +47,9 @@ function media_alunos() {
 // CALCULAR BASKARA
 
 function calcularRaizes() {
-    let a = parseFloat(prompt('Digite 1° Valor: '));
-    let b = parseFloat(prompt('Digite 2° Valor: '));
-    let c = parseFloat(prompt('Digite 3° Valor: '));
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var c = parseFloat(document.getElementById('c').value);
     let delta = b ** 2 - 4 * a * c;
 
     if (delta < 0) {
