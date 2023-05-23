8d686e9ec72b8fba5efde84d6a4dd41066ea0836
Lucas Santos <lucas.souza_santos@live.com>
2023-05-23 15:16:51 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,45 +2,70 @@
 
 function calculararea_circulo(){
     const pi = 3.14
-    let raio = parseInt(document.getElementById('raio').value);
+    let raio = document.getElementById('raio').value;
     let area = (pi * raio* raio).toFixed(2);
-    document.getElementById('resultado').innerHTML = area;
+
+    if (raio == '' || area == '') {
+        document.getElementById('resultado').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('resultado').innerHTML = parseFloat(area);
+    }
 }
 
 // CALCULAR AREA TRIANGULO
 
 function area_hipotenusa(){
-    let cateto1 = parseInt(document.getElementById('cateto1').value);
-    let cateto2 = parseInt(document.getElementById('cateto2').value);
+    let cateto1 = document.getElementById('cateto1').value;
+    let cateto2 = document.getElementById('cateto2').value;
     let hipotenusa = Math.sqrt((cateto1 * cateto2) + (cateto1 * cateto2)).toFixed(2);
-    document.getElementById('resultadohi').innerHTML = hipotenusa;
+
+    if (cateto1 == '' || cateto2 == '') {
+        document.getElementById('resultadohi').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('resultadohi').innerHTML = parseFloat(hipotenusa);
+    }
 }
 
 // CONVERTER REAL EM DOLAR
 
 function real_dolar() {
-    let valor_cotacao = parseInt(document.getElementById('valor_cotacao').value);
-    let valor_reais = parseInt(document.getElementById('valor_reais').value);
+    let valor_cotacao = document.getElementById('valor_cotacao').value;
+    let valor_reais = document.getElementById('valor_reais').value;
     let conversao = (valor_reais / valor_cotacao).toFixed(2);
-    document.getElementById('resultadodolar').innerHTML = conversao;
+
+    if (valor_cotacao == '' || valor_reais == '') {
+        document.getElementById('resultadodolar').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('resultadodolar').innerHTML = parseFloat(conversao);
+    }
 }
 
 // CONVERTER GRAUS CELCIUS EM FAHRENHEIT
 
 function graus_celcius() {
-    let graus_celcius = parseInt(document.getElementById('graus_celcius').value);
+    let graus_celcius = document.getElementById('graus_celcius').value;
     fahrenheit = (graus_celcius * 1.8 + 32).toFixed(2);
-    document.getElementById('resultatempera').innerHTML = fahrenheit;
+
+    if (graus_celcius == '') {
+        document.getElementById('resultatempera').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('resultatempera').innerHTML = parseFloat(fahrenheit);
+    }
 }
 
 // CALCULAR MEDIA ALUNOS
 
 function media_alunos() {
-    let nota1 = parseInt(document.getElementById('nota1').value);
-    let nota2 = parseInt(document.getElementById('nota2').value);
-    let nota3 = parseInt(document.getElementById('nota3').value);
+    let nota1 = document.getElementById('nota1').value;
+    let nota2 = document.getElementById('nota2').value;
+    let nota3 = document.getElementById('nota3').value;
     media_ponderada = ((nota1 + nota2 + nota3) / 3).toFixed(2)
-    document.getElementById('resultadomedia').innerHTML = media_ponderada;
+
+    if (nota1 == '' || nota2 == '' || nota3 == '') {
+        document.getElementById('resultadomedia').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('resultadomedia').innerHTML = parseInt(media_ponderada);
+    }
 }
 
 // CALCULAR BASKARA
@@ -67,7 +92,12 @@ function calcularRaizes() {
 function conversaoVelocidade() {
     let kmhora = document.getElementById('kmhora').value;
     let mSegundo = (kmhora / 3.6).toFixed(2);
-    document.getElementById('conversaokm').innerHTML = mSegundo;
+
+    if (kmhora == '') {
+        document.getElementById('conversaokm').innerHTML = ('Por gentileza digitar um valor a cima.');
+    } else {
+        document.getElementById('conversaokm').innerHTML = parseFloat(mSegundo);
+    }
 }
 
 // Calculo Retangulo
@@ -76,7 +106,12 @@ function calculararea_retangulo() {
     let base = document.getElementById('baseretang').value;
     let altura = document.getElementById('alturaretang').value;
     let resultado = parseFloat(base * altura);
-    document.getElementById('resultadoretang').innerHTML = resultado;
+
+    if (base == '' || altura == '') {
+        document.getElementById('resultadoretang').innerHTML = ('Por gentileza digitar um valor a cima.')
+    } else {
+        document.getElementById('resultadoretang').innerHTML = parseFloat(resultado);
+    }
 }
 
 // Area Triangulo
@@ -85,5 +120,60 @@ function area_triangulo() {
     let basetr = document.getElementById('basetriang').value;
     let alturatr = document.getElementById('alturatriang').value;
     let resultadotriang = (basetr * alturatr) / 2;
-    document.getElementById('resultadotriang').innerHTML = resultadotriang;
+
+    if (basetr == '' || alturatr == '') {
+        document.getElementById('resultadotriang').innerHTML = ('Por gentileza digitar um valor a cima.')
+    } else {
+        document.getElementById('resultadotriang').innerHTML = parseFloat(resultadotriang);
+    }
+}
+
+// Calculadora Cientifica
+
+function calculadorasomar() {
+    let number1 = document.getElementById('numero1').value;
+    let number2 = document.getElementById('numero2').value;
+    let resultadosoma = (number1 + number2);
+
+    if (number1 == '' || number2 == '') {
+        document.getElementById('resultadocalculadora').innerHTML = ('Por gentileza digitar um valor a cima.')
+    } else {
+        document.getElementById('resultadocalculadora').innerHTML = parseInt(resultadosoma)
+    }
+}
+
+function calculadorasubtrair() {
+    let number1 = document.getElementById('numero1').value;
+    let number2 = document.getElementById('numero2').value;
+    let resultadosoma = (number1 - number2);
+
+    if (number1 == '' || number2 == '') {
+        document.getElementById('resultadocalculadora').innerHTML = ('Por gentileza digitar um valor a cima.')
+    } else {
+        document.getElementById('resultadocalculadora').innerHTML = parseInt(resultadosoma)
+    }
+}
+
+function calculadoramultiplicar() {
+    let number1 = document.getElementById('numero1').value;
+    let number2 = document.getElementById('numero2').value;
+    let resultadosoma = (number1 * number2);
+
+    if (number1 == '' || number2 == '') {
+        document.getElementById('resultadocalculadora').innerHTML = ('Por gentileza digitar um valor a cima.')
+    } else {
+        document.getElementById('resultadocalculadora').innerHTML = parseInt(resultadosoma)
+    }
+}
+
+function calculadoradividir() {
+    let number1 = parseInt(document.getElementById('numero1').value);
+    let number2 = parseInt(document.getElementById('numero2').value);
+    let resultadosoma = (number1 / number2);
+
+    if (number1 == 0 || number2 == 0) {
+        document.getElementById('resultadocalculadora').innerHTML = ('Não há divisão por 0');
+    } else {
+        document.getElementById('resultadocalculadora').innerHTML = resultadosoma
+    }
 }
